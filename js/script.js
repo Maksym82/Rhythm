800007b13fe8c8178e3946b342a0af3235eafcf2
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-01-04 17:51:41 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,42 @@ function documentClick(e) {
     document.documentElement.classList.toggle("menu-open");
   }
 }
+
+// Swiper section Advantages
+const swiper = new Swiper(".slider-store", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  grabCursor: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    // when window width is >= 530px
+    530: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    // when window width is >= 1280px
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  }
+});
