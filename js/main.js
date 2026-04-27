1942eab08741953f05b52ab7ef475e837154b9a0
Maksym <112901851+Maksym82@users.noreply.github.com>
2026-04-27 10:43:13 +0300

    Update styles

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ document.addEventListener('click', documentClick);
 
 function documentClick(e) {
   const targetItem = e.target;
-  console.log(targetItem);
   if (targetItem.closest('.icon-menu')) {
     document.documentElement.classList.toggle('menu-open');
   } else if (targetItem.closest('.menu__link')) {
